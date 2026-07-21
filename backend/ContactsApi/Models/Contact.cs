9f4ff8afa48fc2cd451e254a47f23ec5b015402f
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-07-21 11:06:41 +0200

    contact model class

---
+++ backend/ContactsApi/Models/Contact.cs
@@ -0,0 +1,23 @@
+namespace ContactsApi.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Phone { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+
+    public required int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
+    public int? SubcategoryId { get; set; }
+    public SubCategory? SubCategory { get; set; }
+
+    public string? CustomSubcategory { get; set; }
+
+    public required int OwnerId { get; set; }
+    public User Owner { get; set; } = null!;
+}
